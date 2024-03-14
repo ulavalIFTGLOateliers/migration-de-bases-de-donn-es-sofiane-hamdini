c6c509bb07aa5e4e105a4a8e30927d0ed10621a4
sofiane-hamdini <151280220+sofiane-hamdini@users.noreply.github.com>
2024-03-13 20:21:47 -0400

    fixed constraint issue

--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,10 +1,11 @@
 DROP TABLE band;
 
+ALTER TABLE album DROP CONSTRAINT FK_A_singerName;
+
 ALTER TABLE musician RENAME TO singer;
 ALTER TABLE singer RENAME COLUMN musicianNAME TO singerName;
 ALTER TABLE singer DROP COLUMN role;
 ALTER TABLE singer DROP COLUMN bandName;
 
-ALTER TABLE album DROP CONSTRAINT FK_A_singerName;
 ALTER TABLE album
     ADD CONSTRAINT FK_A_singerName FOREIGN KEY (singerName) REFERENCES singer (singerName);
